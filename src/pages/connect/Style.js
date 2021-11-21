178edc1e51d560049bc7b0b96e0a7239c6dc4d47
chuksdozie <chuksdozie48@gmail.com>
2021-11-21 09:43:11 +0100

    onboarding page, link activation for buttons

---
+++ src/pages/connect/Style.js
@@ -0,0 +1,11 @@
+export const main = {
+  width: "100vw",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  backgroundColor: "#f2f2f2",
+};
+
+export const link = {
+  width: "100%", textDecoration: "none" 
+}
